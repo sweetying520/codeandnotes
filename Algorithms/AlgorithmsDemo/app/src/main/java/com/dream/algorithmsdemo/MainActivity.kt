fe52824db30cd41be4f936a977e8ef6f4a1485db
zhouying <1770385837@qq.com>
2022-05-12 17:54:20 +0800

    feat: Initial commit

---
+++ Algorithms/AlgorithmsDemo/app/src/main/java/com/dream/algorithmsdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dream.algorithmsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
